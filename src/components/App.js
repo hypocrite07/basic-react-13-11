beeb9ecfa9a5c7d5f94c5d9404d7417599fbfbd6
agon <agoncharov@esphere.ru>
2017-11-26 21:36:45 +0300

    ht-3.3

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,14 @@ import ArticlesChart from './ArticlesChart'
 import UserForm from './UserForm'
 import Select from 'react-select'
 import 'react-select/dist/react-select.css'
+import DayPicker from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
+import * as _ from 'lodash'
 
 class App extends Component {
     state = {
-        selected: null
+        selected: null,
+        selectedDate: ''
     }
 
     handleSelect = selected => this.setState({ selected })
@@ -18,15 +22,25 @@ class App extends Component {
             label: article.title,
             value: article.id
         }))
+
         return (
             <div>
                 <h1>App name</h1>
                 <UserForm />
                 <Select options = {options} value = {this.state.selected} onChange = {this.handleSelect} multi />
+                <DayPicker onDayClick={this.onDaySelect}/>
+                <input value={this.state.selectedDate}></input>
                 <ArticleList articles = {articles} defaultOpenId = {articles[0].id}/>
                 <ArticlesChart articles = {articles} />
             </div>
         )
+    }
+
+    onDaySelect = date => {
+        const newState = _.extend(this.state, {
+            selectedDate: date.toDateString()
+        })
+        this.setState(newState)
     }
 }
 
